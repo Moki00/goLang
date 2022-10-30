21d92f7fb399c6c36178df2676debb39d2147c9c
Moki <moking929@gmail.com>
2022-10-30 10:37:54 -0400

    codewars

--- example/example.go
+++ example/example.go
@@ -2,12 +2,23 @@ package main
 
 import (
     "fmt"
-
-    "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+    fmt.Println(Points([]string{"1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"}))
+}
+
+func Points(games []string) int {
+  
+  sum:=0
+  
+  for _, v := range games {
+
+    var s = int(v[0])
+    sum+= 1
+    fmt.Println(s)
+    fmt.Printf("we see %v\n", v)
+  }
+  
+  return sum
 }
